1bef329d838dd2933f6757bdf03611354e0484c3
Christine Stone <christinestone932@gmail.com>
2020-01-27 18:44:18 +1100

    Updates

---
+++ precache-manifest.a2a60a275a43ea87f71bec05b2ad51fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e533ff511c44dee625e11bf9b8fbafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90152d139053a65c201c",
+    "url": "/static/css/main.81fbacba.chunk.css"
+  },
+  {
+    "revision": "cf55120fff752f0eab74",
+    "url": "/static/js/2.4d2eb210.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4d2eb210.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90152d139053a65c201c",
+    "url": "/static/js/main.a99a6830.chunk.js"
+  },
+  {
+    "revision": "5768baf0182c8dc3a3de",
+    "url": "/static/js/runtime-main.96f48b07.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/static/media/sprites.6a398990.png"
+  }
+]);
