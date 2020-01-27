505b996761ac30e0fe06b15faf8b6133f7306a07
Christine Stone <christinestone932@gmail.com>
2020-01-27 18:19:38 +1100

    add view components and data

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PokeList from '../components/PokeList';
+import DetailView from '../components/DetailView';
+import Pokemon from '../components/Pokemon';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+        pokemon: {}
+    }
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          const pokemon = new Pokemon(data);
+          this.setState({ pokemon });
+        })
+  }
+
+  render() {
+    const { pokemon } = this.state;
+    return (
+            <div className='App'>
+              <PokeList handleOnClick={this.handleOnClick} />
+              <DetailView pokemon={pokemon} />
+            </div>
+        )
+  }
 }
 
 export default App;
